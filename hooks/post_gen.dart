e4efd9b47fb20f584f5e5610fa93e2819f219f99
Jia Xing Wan <jxwan98@gmail.com>
2024-12-29 20:00:15 +0800

    Added runInShell to fix ProcessException: The system cannot find the file specified

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -8,14 +8,14 @@ Future<void> run(HookContext context) async {
   try {
     // ! Run `flutter pub get`
     context.logger.info('Fetching dependencies...');
-    final pubGetResult = await Process.run('flutter', ['pub', 'get']);
+    final pubGetResult = await Process.run('flutter', ['pub', 'get'], runInShell: true);
     stdout.write(pubGetResult.stdout);
     stderr.write(pubGetResult.stderr);
 
     // ! Run `flutter pub run flutter_flavorizr`
     context.logger.info('Generating flavors...');
 
-    final flavorizrResult = await Process.run('flutter', ['pub', 'run', 'flutter_flavorizr']);
+    final flavorizrResult = await Process.run('flutter', ['pub', 'run', 'flutter_flavorizr'], runInShell: true);
     stdout.write(flavorizrResult.stdout);
     stderr.write(flavorizrResult.stderr);
 
@@ -26,14 +26,15 @@ Future<void> run(HookContext context) async {
     // ! Run `flutter gen-l10n`
     context.logger.info('Generating l10n app_localizations.dart...');
 
-    final l10nResult = await Process.run('flutter', ['gen-l10n']);
+    final l10nResult = await Process.run('flutter', ['gen-l10n'], runInShell: true);
     stdout.write(l10nResult.stdout);
     stderr.write(l10nResult.stderr);
 
     if (l10nResult.exitCode == 0) {
       context.logger.info('l10n app_localizations setup complete!');
     }
-  } catch (e) {
+  } catch (e, stacktrace) {
     context.logger.err('Error during post-generation: $e');
+    context.logger.err('Stacktrace:\n$stacktrace');
   }
 }
