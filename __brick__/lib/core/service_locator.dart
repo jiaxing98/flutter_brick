5e3acc5c3fd194331eda026427897a8be66bcdf7
WAN JIA XING <jiaxing.wan@redone.com.my>
2025-02-07 11:11:06 +0800

    Added hydrated_bloc and sqflite, update bloc version

--- __brick__/lib/core/service_locator.dart
+++ __brick__/lib/core/service_locator.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:{{project_name}}/core/env.dart';
 import 'package:{{project_name}}/core/l10n/l10n.dart';
@@ -8,6 +10,10 @@ import 'package:{{project_name}}/core/theme/theme.dart';
 GetIt sl = GetIt.instance;
 
 Future<void> initializedApp() async {
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: HydratedStorageDirectory((await getTemporaryDirectory()).path),
+  );
+
   final sharedPreferences = await SharedPreferences.getInstance();
   sl.registerSingleton<SharedPreferences>(sharedPreferences);
 
